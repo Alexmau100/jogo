03c8f90a9a2a17ca4bdca4eda9c88015c3650fc8
Alexmau100 <alexandremauro.rjo@gmail.com>
2025-03-14 15:23:15 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = ["Alex", "Cris", "Igor", "Guga"];  
+
+function adicionarAmigo() { 
+    const nomeInput = document.getElementById('nomeInput').value;  
+
+    if (nomeInput) {  
+        amigos.push(nomeInput);  
+        document.getElementById('nomeInput').value = '';  
+        atualizarLista();  
+    } else {
+        alert("Por favor, insira um nome.");
+    }
+}
+
+function atualizarLista() {
+    const listaElement = document.getElementById('listaAmigos');
+    listaElement.innerHTML = '';  
+
+    amigos.forEach(amigo => {
+        const li = document.createElement('li');
+        li.textContent = amigo;
+        listaElement.appendChild(li);  
+    });
+}
+
+function sortearAmigo() {
+    if (amigos.length < 4) {  
+        alert("Não há amigos suficientes para sortear. Adicione mais amigos!");
+        return;  
+    }
+
+    
+    const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    const amigoSorteado = amigos[indiceAleatorio];
+    
+    
+    alert("O amigo sorteado é: " + amigoSorteado);
+}
+
